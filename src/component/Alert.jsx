feba629c3d1ab3af421598f82d0bb2bdb3bab484
Nandu-Chopade <nanduchopade1998@gmail.com>
2023-07-24 12:18:12 +0530

    Routing added to project

--- src/component/Alert.jsx
+++ src/component/Alert.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function Alert(props) {
   return (
-    props.message && <div class="alert alert-primary" role="alert">
-    <strong>{props.message.type}: </strong>  {props.message.message} 
+    <div style={{ height: "50px" }}>
+      {props.message &&
+      <div class="alert alert-primary" role="alert">
+        <strong>{props.message.type}: </strong> {props.message.message}
+      </div>}
     </div>
   );
 }
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,93 +1,56 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
-    <>
-      <nav
-        className={`navbar navbar-expand-lg  navbar-${props.mode} bg-${props.mode}`}
-      >
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            {props.title}
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          {props.title}
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <div className="form-check form-switch">
               <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
+                className="form-check-input my-1"
+                onClick={props.toggleMode}
+                type="checkbox"
+                id="flexSwitchCheckDefault"
               />
-    <div className="form-check form-switch">
-    <input class="form-check-input my-1"  onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault" />
-                
-              </div>
-              {/* <button
+            </div>
+            {/* Uncomment the button below if you want to include it */}
+            {/* <button
                 className="btn btn-outline-primary light"
-                
                 type="submit"
               >
                 Mode
-              </button> */}
-              
-            </form>
-          </div>
+            </button> */}
+          </form>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 }
